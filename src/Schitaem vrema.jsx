1426cb0a8a824785650e0837666db7c5c524446c
ockap911 <ockap911@gmail.com>
2025-11-27 15:52:27 +0000

    Таймер с масивом времени

--- src/Schitaem vrema.jsx
+++ src/Schitaem vrema.jsx
@@ -3,19 +3,23 @@ import { Button } from 'antd';
 
 function Vrema() {
 
-  let [Rolov, setRolov] = useState();
+  let [Rolov, setRolov] = useState([]);
 
   function stoi(){
     
   const timeValue = new Date();
   const pokaz_vrema = timeValue.getHours() + ' : ' + timeValue.getMinutes() + ' : ' + timeValue.getSeconds();
-  setRolov(pokaz_vrema);
+  
+  setRolov([...Rolov, pokaz_vrema]);
+  // setRolov(Rolov.push(pokaz_vrema));
 
   }
     return (
     <div>
       <Button onClick={stoi} >Сколько времени, дАрАгой</Button>
-      <div>{Rolov}</div>
+      <div>{Rolov.map((item, index) => {
+        return <div key={index}>{item}</div>
+      })}</div>
     </div>
 
     )
